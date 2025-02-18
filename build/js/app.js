6cb193d278df6c076adca593f8e5571bcbde4d80
Alexis Eduardo <eduardo.7493z@gmail.com>
2025-02-18 00:30:33 -0600

    Added first version of dark teme

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     navFixed();
     eventListeners();
+    darkMode();
     //scrollNav();
 });
 
@@ -40,4 +41,43 @@ function navegacionResponsive() {
 
     //o una mas corta 
     //navegacion.classList.toggle('mostrar');
+}
+
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+      } else if (localStorage.getItem('modo-oscuro') === 'false') {
+        document.body.classList.remove('dark-mode');
+      } else {
+//
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro', 'true');
+        } else {
+            localStorage.setItem('modo-oscuro', 'false');
+        }
+    });
 }
